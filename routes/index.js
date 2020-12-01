3d267c160683af63a9f2120d4eccfb2356f9ddcb
Type9 <gabespersonal@gmail.com>
2020-12-02 07:50:29 +0800

    Added search pagination

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
     const page = req.query.page || 1
   
     Pet.paginate({}, {page: page}).then((results) => {
-      res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
+      res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page, term: req.query.term });
     });
   });
 }
