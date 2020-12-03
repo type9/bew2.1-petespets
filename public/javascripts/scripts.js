e09c68fc0c0f8f449083095ae8287dd82b426f69
Type9 <gabespersonal@gmail.com>
2020-12-04 07:42:08 +0800

    Implemented S3 file uploads

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,29 +1,24 @@
 if (document.querySelector('#new-pet')) {
     document.querySelector('#new-pet').addEventListener('submit', (e) => {
         e.preventDefault();
+        // Use FormData to grab everything now that we have files mixed in with text
+        var form = document.getElementById("new-pet");
+        var pet = new FormData(form);
 
-        let pet = {};
-        const inputs = document.querySelectorAll('.form-control');
-        for (const input of inputs) {
-            pet[input.name] = input.value;
-        }
-
-        axios.post('/pets', pet)
+        // Assign the multipart/form-data headers to axios does a proper post
+        axios.post('/pets', pet, {
+            headers: {
+                'Content-Type': 'multipart/form-data;'
+            }
+        })
             .then(function (response) {
                 window.location.replace(`/pets/${response.data.pet._id}`);
             })
             .catch(function (error) {
-                axios.post('/pets', pet)
-                    .then(function (response) {
-                        window.location.replace(`/pets/${response.data._id}`);
-                    })
-                    // New Catch Code
-                    .catch(function (error) {
-                        const alert = document.getElementById('alert')
-                        alert.classList.add('alert-warning');
-                        alert.textContent = 'Oops, something went wrong saving your pet. Please check your information and try again.';
-                        alert.style.display = 'block';
-                    });
+                const alert = document.getElementById('alert')
+                alert.classList.add('alert-warning');
+                alert.textContent = 'Oops, something went wrong saving your pet. Please check your information and try again.';
+                alert.style.display = 'block';
                 setTimeout(() => {
                     alert.style.display = 'none';
                     alert.classList.remove('alert-warning');
